b35af4b607878d71bfd4e69652e8467e658e8eee
Wasp <aworknightowl@gmail.com>
2018-03-30 11:29:15 -0400

    Update index.php

--- PHP/index.php
+++ PHP/index.php
@@ -18,10 +18,10 @@
 </form>
 <?php
     //login
-    define('DB_HOST', 'localhost');
-    define('DB_USER', 'root');
-    define('DB_PASS', '????');
-    define('DB_NAME', 'Main');
+    define('DB_HOST', '');
+    define('DB_USER', '');
+    define('DB_PASS', '');
+    define('DB_NAME', '');
 
     $dbConnection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
@@ -39,29 +39,36 @@
     mysqli_close($dbConnection);
 
 ?>
+    //get the answer
 <?php
-    //login
-    define('DB_HOST1', 'localhost');
-    define('DB_USER1', 'root');
-    define('DB_PASS1', '????');
-    define('DB_NAME1', 'Main');
+    define('DB_HOST', '');
+    define('DB_USER', '');
+    define('DB_PASS', '');
+    define('DB_NAME', '');
 
-    $dbConnection1 = mysqli_connect(DB_HOST1, DB_USER1, DB_PASS1, DB_NAME1);
-    $query = "SELECT * FROM USERINFO";
+    $dbConnection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    $query = "SELECT problem_title FROM table_name";
     // here we save mysqli_query in result
-    $result = mysqli_query($dbConnection1, $query);
-    $row = mysqli_fetch_array($result);
+    $result = mysqli_query($dbConnection, $query);
+    $colums = mysqli_num_fields($result);   //get colums
     // show the result in web page
-    $t_user = $row['t_user'];
-    echo sizeof($row['t_user']);
-    for ($i = 0; $i <sizeof($t_user);$i++)
-    {
+    echo "<table>";
+    //print the first row with title
+    echo "<tr>";
+    for($i = 0; $i < $colums; $i++){
+        $field_name = mysqli_field_name($result, $i);
+        echo "<th>$field_name</th>";
+    }
+    echo "</tr>";
+    // if there has row, we print that row
+    while($row = mysqli_fetch_row($result)){
         echo "<tr>";
-        echo "<td>".$t_user."</td>";
+        for ($i = 0; $i < $colums; $i++){
+            echo "<td>$row[$i]</td>td>";
+        }
         echo "</tr>";
     }
-
-    mysqli_close($dbConnection1);
+    echo "</table>"
 ?>
 </body>
 </html>
