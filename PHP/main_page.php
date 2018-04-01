3fc8cc81cd9fdac1189cbd688932f59438829943
Wasp <aworknightowl@gmail.com>
2018-04-01 12:52:55 -0400

    Create main_page.php

---
+++ PHP/main_page.php
@@ -0,0 +1,14 @@
+<?php session_start();
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Management System</title>
+</head>
+<body>
+<h2>Management System</h2>
+<h3>Employee Management</h3>
+<a href="add_employee.php">add employee</a><br/>
+<a href="show_employee.php">show employee</a>
+</body>
+</html>
